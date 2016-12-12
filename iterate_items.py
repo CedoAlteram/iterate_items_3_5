86d9373452b2598c7cab5617b9766241b35471b6
centurion <aaron.reyna@gmail.com>
2016-12-12 13:12:22 -0800

    Initial commit

---
+++ iterate_items.py
@@ -0,0 +1,11 @@
+import collections
+
+a = ['EM', 'BM', 'IX', 'CI', 'EX', 'DI', 'CX', 'DM', 'DX', 'BI', 'CE', 'JC', 'IX', 'JM', 'BM', 'EI', 'DI', 'EM', 'DM', 'EX', 'CX', 'CE', 'JC', 'CM', 'DX', 'BM', 'JX']
+
+counts = collections.defaultdict(int)
+for letters in a:
+    counts[letters]+=1
+
+print (counts)
+hist = dict( (k, float(v)/len(a)) for k,v in counts.items())
+print (hist)
